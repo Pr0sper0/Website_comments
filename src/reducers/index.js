9723547005c2a58b02c4c46b86191c05c1f0967e
Val <gestor009@gmail.com>
2018-09-22 14:35:57 +0300

    reducers and actions updated

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import Postsreducer from './reducer_posts';
+
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  posts: Postsreducer,
+  form: formReducer
 });
 
 export default rootReducer;
